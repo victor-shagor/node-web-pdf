aa8c1e474f20801c7fd52da5595f4cea38d8c93b
victor-shagor <sadeoye@extracker.com>
2023-03-07 07:52:19 +0100

    com

--- src/databaseConnection.ts
+++ src/databaseConnection.ts
@@ -3,12 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { DATABASE_URL, DATABASE_NAME } = process.env;
+const { DATABASE_URL } = process.env;
 
 const connectToDatabase = async () => {
   const options = { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true };
 
-  await mongoose.connect(`${DATABASE_URL}/${DATABASE_NAME}`);
+  await mongoose.connect(`${DATABASE_URL}`);
+  
 };
 
 export { connectToDatabase };
